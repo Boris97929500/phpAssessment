35a20eaeae3872573f97038a7f4372bbf8d98c78
Boris <boris@email.com>
2022-04-17 16:04:50 +1000

    added more functionality with secondary keys

--- README.txt
+++ README.txt
@@ -9,13 +9,6 @@ CREATE TABLE users(
 
 
 
-CREATE TABLE posts(
-    id int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    title VARCHAR(255)  NOT NULL,
-    imageurl VARCHAR(255) NOT NULL,
-    comments VARCHAR(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
-
 
 
 INSERT INTO users (idUsers, uidUsers, emailUsers, pwdUsers) VALUES(
@@ -29,6 +22,55 @@ INSERT INTO users (idUsers, uidUsers, emailUsers, pwdUsers) VALUES(
     'max@hotmail.com',
     '123'
 );
+
+
+
+//if have time, can try develop secondary key
+CREATE TABLE posts(
+    id int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(255)  NOT NULL,
+    imageurl VARCHAR(255) NOT NULL,
+    comments VARCHAR(255) NOT NULL,
+    users_id INT,
+    FOREIGN KEY(users_id) REFERENCES users(idUsers)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+please create a fifth user to test the edit and delete functionality for these original posts
+
+INSERT INTO posts ( title, imageurl, comments, users_id) VALUES(
+
+    'car one',
+    'https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=452&q=80',
+    'awesome car',
+    5
+),(
+
+    'car two',
+    'https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    'another car',
+    1
+),(
+
+    'car three',
+    'https://images.unsplash.com/photo-1542362567-b07e54358753?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    'third car',
+    5
+);
+
+
+
+
+//simpler version without secondary keys
+
+
+CREATE TABLE posts(
+    id int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(255)  NOT NULL,
+    imageurl VARCHAR(255) NOT NULL,
+    comments VARCHAR(255) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+
 
 INSERT INTO posts ( title, imageurl, comments) VALUES(
 
--- posts.php
+++ posts.php
@@ -19,8 +19,98 @@
 <!-- image body -->
 <div class="container">
     <h2>Read Post</h2>
-</div>
+    <main class="container " style="width: 80%">
+    <?php
 
+      // (i) Connect to Database
+      require './includes/connect.inc.php';
+
+      // (ii) create SQL commands
+      $sql = "SELECT id, title, imageurl , comments, users_id FROM posts";
+
+      // (iii) Call query & store result in variable
+      $result = mysqli_query($conn, $sql);
+    ?>
+
+    <?php 
+    // show success alert
+      if(isset($_GET['post']) == "success"){
+        echo '<div class="alert alert-success" role="alert">
+          Post created!
+        </div>';  
+      } else if(isset($_GET['edit']) == "success"){
+        echo '<div class="alert alert-success" role="alert">
+          Post edited!
+        </div>'; 
+      }
+
+      // show error alert
+      if(isset($_GET['error'])){
+        // (i) Internal server error 
+        if ($_GET['error'] == "sqlerror") {
+          $errorMsg = "An internal server error has occurred ";
+        }
+
+        // (ii) Dynamic Error Alert based on Variable Value 
+        echo '<div class="alert alert-danger" role="alert">' . $errorMsg . '</div>';
+
+      // (iii) Display SUCCESS message for correct login
+      } else if (isset($_GET['delete']) == "success"){
+        echo '<div class="alert alert-success" role="alert">
+          Post successfully deleted!
+        </div>';    
+      }
+    ?>
+
+    <?php
+
+      // (2.i) Success: Display Posts
+      if(mysqli_num_rows($result) > 0){
+
+        // 3. loop data card template
+        // (3.i) New variable with default state
+        $output = "";
+
+        // (3.ii) Take result -> convert to array & then insert into While Loop
+        while($row = mysqli_fetch_assoc($result)) {
+
+         $output .= 
+
+          '
+            <div class="card border-0 mt-3" id="' . $row['id'] . '">
+              <img src="' . $row['imageurl'] . '" class="card-img-top post-image" alt="' . $row['title'] . '">
+              <div class="card-body">
+                <h5 class="card-title">' . $row['title'] . '</h5>
+                <p class="card-text">' . $row['comments'] . '</p> ';
+                
+                //Restrict Edit/Delete Button only to users who created the post 
+                if(isset($_SESSION['userId']) &&  $_SESSION['userId'] == $row['users_id']){
+
+                  $output .= '
+                  <div class="admin-btn">
+                    <a href="editpost.php?id=' . $row['id'] . '" class="btn btn-secondary mt-2">Edit</a>
+                    <a href="includes/deletepost.inc.php?id=' . $row['id'] . '" class="btn btn-danger mt-2">Delete</a>
+                  </div>';
+                }
+
+            $output .= 
+            '
+              </div>
+            </div>
+            ';
+        }
+        // (3.iii) Echo out the result of the loop
+        echo $output;
+
+      } else {
+        echo "0 results";
+      }
+      // (2.iii) Close Connection
+      mysqli_close($conn);
+    ?>
+  </main>
+
+</div>
 
 <!--  footer -->
 <?php  require "footer.php" ?>
