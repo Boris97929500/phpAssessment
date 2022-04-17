a00d7c8c8a227b612b9e0dc26ccf00a39c8178d4
Boris <boris@email.com>
2022-04-17 13:30:07 +1000

    added sessions and log out

--- header.php
+++ header.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+?>
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,12 +19,25 @@
       <li class="nav-item">
         <a class="nav-link" href="signup.php">Signup</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="login.php">Logout</a>
-      </li>
+        <?php
+        if(isset($_SESSION['userId'])){
+            echo '
+            <li class="nav-item">
+            <form action="includes/logout.inc.php" action="POST">
+                <button type="submit" class="btn btn-primary" name="logout-submit">Log out</button>
+            </form>
+            </li>
+            
+            ';
+        }else{
+            echo '
+            <li class="nav-item">
+            <a class="nav-link" href="login.php">Login</a>
+            </li>
+            ';
+        }
+
+        ?>
     </ul>
   </div>
 </nav>
